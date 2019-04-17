6552fed6570bf19fded8dba0a065c514fed31d2c
djankowski <dennis.jankowski95@icloud.com>
2019-04-17 12:15:48 +0200

    interpolating numbers based on a simple data series

--- simpleInterpolation.py
+++ simpleInterpolation.py
@@ -11,15 +11,27 @@ This relationship is recognized so that new inputs (to_predict) can be predicted
 model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-xs=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-ys=[3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+# data series without gap from 5 to 7
+xs=[1, 2, 3, 4, 8, 9, 10]
+ys=[4, 8, 12, 16, 32, 36, 40]
 
 model.fit(xs, ys, epochs=4000)
 
-to_predict = 50
-result = model.predict([to_predict])
-rounded_result = np.round(result[0][0], 2)
+to_predict_0 = 5
+result_0 = model.predict([to_predict_0])
+rounded_result_0 = np.round(result_0[0][0], 2)
+
+to_predict_1 = 6
+result_1 = model.predict([to_predict_1])
+rounded_result_1 = np.round(result_1[0][0], 2)
+
+to_predict_2 = 7
+result_2 = model.predict([to_predict_2])
+rounded_result_2 = np.round(result_2[0][0], 2)
 
 print(50 * '-')
 
-print(rounded_result)
+print(rounded_result_0)
+print(rounded_result_1)
+print(rounded_result_2)
+
