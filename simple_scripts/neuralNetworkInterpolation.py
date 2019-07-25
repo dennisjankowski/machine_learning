a9cb8e7e4c6c8d8951c78ffbb54e757aa71679a2
Dennis <d.jankowski95@web.de>
2019-07-25 19:29:10 +0200

    a NN which should be able to interpolate ais data. (not finished yet, results are quiet bad)

--- simple_scripts/neuralNetworkInterpolation.py
+++ simple_scripts/neuralNetworkInterpolation.py
@@ -4,19 +4,24 @@ import random
 import dateutil
 from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
 
-'''
-This NN learns simple relationships between two series of numbers. 
-In this case, the second series of numbers is always three times the first. 
-This relationship is recognized so that new inputs (to_predict) can be predicted based on the model.
-'''
 
 def remove_random_entries(l, n):
     for _ in range(int(len(l) * n)):
         l.pop(random.randrange(0, len(l)))
     return l
 
-model = keras.Sequential([keras.layers.Dense(units=300, input_shape=[2])])
+'''
+This NN learns simple relationships between two series of numbers. 
+'''
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(units=5000, input_shape=[1]))
+model.add(keras.layers.Dense(units=10000, input_shape=[5000]))
+model.add(keras.layers.Dense(units=15000, input_shape=[10000]))
+#model.add(keras.layers.Dense(units=2000, input_shape=[5000], activation=keras.activations.softmax))
+
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 csv_file = csv.DictReader(open(file='../resources/trajectory.csv'))
@@ -41,53 +46,30 @@ coordinates_list = list(zip(timestamp_list, latitude_list, longitude_list))
 coordinates_list = list(set(coordinates_list))
 
 
-# copy the coordinates list and remove x percent of entries
-coordinates_list_less = coordinates_list[:]
-
 # sort the lists by timestamps and unzip the triples into three seperate lists
 coordinates_list.sort()
 coordinates_list = remove_random_entries(coordinates_list, 0.9)
+coordinates_list = coordinates_list[-50:]
+
 coordinates_list = list(zip(*coordinates_list))
 
 timestamp_list = coordinates_list[0]
 latitude_list = coordinates_list[1]
 longitude_list = coordinates_list[2]
 
-time_list = []
-counter = 0
-for x in range(95):
-    time_list.append(counter)
-    counter = counter + 1
-
-timestamp_list = np.array(timestamp_list)
-latitude_list = np.array(latitude_list)
-longitude_list = np.array(longitude_list)
-input_list = np.array(list(zip(latitude_list, time_list)))
-
-print(input_list)
-
-xs1 = latitude_list
-xs2 = timestamp_list
+xs = latitude_list
 ys = longitude_list
 
-model.fit(input_list, ys, epochs=5000)
+model.fit(xs, ys, epochs=20)
 
-
-to_predict_0 = [53.863, 16.5]
-test = [to_predict_0]
-test = np.array(test)
-result_0 = model.predict(test)
-
-print('result:', result_0[0][0])
-
-import matplotlib.pyplot as plt
-
-plt.plot(xs1, ys, 'ro', color='blue')
-plt.plot(to_predict_0[0], [result_0[0][0]], 'ro')
-plt.show()
+to_predict_0 = 53.88
+result_0 = model.predict([to_predict_0])
+print(result_0)
+rounded_result_0 = np.round(result_0[0][0], 2)
 
 print(50 * '-')
+print(rounded_result_0)
 
-#print(rounded_result_0)
-#print(rounded_result_1)
-#print(rounded_result_2)
+plt.plot(xs, ys, 'ro', color='blue')
+plt.plot([to_predict_0], [rounded_result_0], 'ro')
+plt.show()
